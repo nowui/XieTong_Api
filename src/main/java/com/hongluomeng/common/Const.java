d00d42e8fd8b7724236a9ec23774ca1e0bc63329
NowUI <nowui@163.com>
2016-09-16 15:11:33 +0800

    2016-09-16 15:11

--- src/main/java/com/hongluomeng/common/Const.java
+++ src/main/java/com/hongluomeng/common/Const.java
@@ -54,6 +54,7 @@ public class Const {
 	public static final String URL_ADMIN_OPERATION_LIST = "/admin/operation/list";
 	public static final String URL_ADMIN_OPERATION_UPDATE = "/admin/operation/update";
 	public static final String URL_ADMIN_LOGIN = "/admin/login";
+
 	public static final String URL_ATTRIBUTE_LIST = "/attribute/list";
 	public static final String URL_ATTRIBUTE_FIND = "/attribute/find";
 	public static final String URL_ATTRIBUTE_SAVE = "/attribute/save";
@@ -61,23 +62,28 @@ public class Const {
 	public static final String URL_ATTRIBUTE_DELETE = "/attribute/delete";
 	public static final String URL_AUTHORIZATION_LIST = "/authorization/list";
 	public static final String URL_AUTHORIZATION_FIND = "/authorization/find";
+
 	public static final String URL_BRAND_LIST = "/brand/list";
 	public static final String URL_BRAND_FIND = "/brand/find";
 	public static final String URL_BRAND_SAVE = "/brand/save";
 	public static final String URL_BRAND_UPDATE = "/brand/update";
 	public static final String URL_BRAND_DELETE = "/brand/delete";
+
 	public static final String URL_CATEGORY_LIST = "/category/list";
 	public static final String URL_CATEGORY_FIND = "/category/find";
 	public static final String URL_CATEGORY_SAVE = "/category/save";
 	public static final String URL_CATEGORY_UPDATE = "/category/update";
 	public static final String URL_CATEGORY_DELETE = "/category/delete";
+
 	public static final String URL_GROUP_LIST = "/group/list";
 	public static final String URL_GROUP_FIND = "/group/find";
 	public static final String URL_GROUP_SAVE = "/group/save";
 	public static final String URL_GROUP_UPDATE = "/group/update";
 	public static final String URL_GROUP_DELETE = "/group/delete";
+
 	public static final String URL_LOG_LIST = "/log/list";
 	public static final String URL_LOG_FIND = "/log/find";
+
 	public static final String URL_MEMBER_LIST = "/member/list";
 	public static final String URL_MEMBER_FIND = "/member/find";
 	public static final String URL_MEMBER_DELETE = "/member/delete";
@@ -90,16 +96,19 @@ public class Const {
 	public static final String URL_MEMBER_NAME_UPDATE = "/member/name/update";
 	public static final String URL_MEMBER_PASSWORD_UPDATE = "/member/password/update";
 	public static final String URL_MEMBER_AVATAR_UPLOAD = "/member/avatar/upload";
+
 	public static final String URL_MENU_LIST = "/menu/list";
 	public static final String URL_MENU_FIND = "/menu/find";
 	public static final String URL_MENU_SAVE = "/menu/save";
 	public static final String URL_MENU_UPDATE = "/menu/update";
 	public static final String URL_MENU_DELETE = "/menu/delete";
+
 	public static final String URL_OPERATION_LIST = "/operation/list";
 	public static final String URL_OPERATION_FIND = "/operation/find";
 	public static final String URL_OPERATION_SAVE = "/operation/save";
 	public static final String URL_OPERATION_UPDATE = "/operation/update";
 	public static final String URL_OPERATION_DELETE = "/operation/delete";
+
 	public static final String URL_PRODUCT_LIST = "/product/list";
 	public static final String URL_PRODUCT_FIND = "/product/find";
 	public static final String URL_PRODUCT_SAVE = "/product/save";
@@ -115,6 +124,7 @@ public class Const {
 	public static final String URL_PRODUCT_CATEGORY_ATTRIBUTE_SAVE = "/product/category/attribute/save";
 	public static final String URL_PRODUC_CATEGORYT_ATTRIBUTE_UPDATE = "/product/category/attribute/update";
 	public static final String URL_PRODUC_CATEGORYT_ATTRIBUTE_DELETE = "/product/category/attribute/delete";
+
 	public static final String URL_ROLE_LIST = "/role/list";
 	public static final String URL_ROLE_FIND = "/role/find";
 	public static final String URL_ROLE_SAVE = "/role/save";
@@ -122,22 +132,28 @@ public class Const {
 	public static final String URL_ROLE_DELETE = "/role/delete";
 	public static final String URL_ROLE_OPERATION_LIST = "/role/operation/list";
 	public static final String URL_ROLE_OPERATION_UPDATE = "/role/operation/update";
+
 	public static final String URL_SHOP_LIST = "/shop/list";
 	public static final String URL_SHOP_FIND = "/shop/find";
 	public static final String URL_SHOP_SAVE = "/shop/save";
 	public static final String URL_SHOP_UPDATE = "/shop/update";
 	public static final String URL_SHOP_DELETE = "/shop/delete";
+
 	public static final String URL_UPLOAD_LIST = "/upload/list";
 	public static final String URL_UPLOAD_IMAGE = "/upload/image";
+
 	public static final String URL_USER_MENU_LIST = "/user/menu/list";
+
 	public static final String URL_SMS_REGISTER = "/sms/register";
 	public static final String URL_SMS_RESET_PASSWORD = "/sms/resetpassword";
+
 	public static final String URL_TEACHER_LIST = "/teacher/list";
 	public static final String URL_TEACHER_FIND = "/teacher/find";
 	public static final String URL_TEACHER_SAVE = "/teacher/save";
 	public static final String URL_TEACHER_UPDATE = "/teacher/update";
 	public static final String URL_TEACHER_DELETE = "/teacher/delete";
 	public static final String URL_TEACHER_LOGIN = "/teacher/login";
+
 	public static final String URL_STUDENT_LIST = "/student/list";
 	public static final String URL_STUDENT_FIND = "/student/find";
 	public static final String URL_STUDENT_SAVE = "/student/save";
@@ -148,12 +164,14 @@ public class Const {
 	public static final String URL_STUDENT_UPLOAD = "/student/upload";
 	public static final String URL_STUDENT_DELETE_2 = "/student/delete2";
 	public static final String URL_STUDENT_PASSWORD_UPDATE = "/student/password/update";
+
 	public static final String URL_GRADE_LIST = "/grade/list";
 	public static final String URL_GRADE_FIND = "/grade/find";
 	public static final String URL_GRADE_SAVE = "/grade/save";
 	public static final String URL_GRADE_UPDATE = "/grade/update";
 	public static final String URL_GRADE_DELETE = "/grade/delete";
 	public static final String URL_GRADE_LIST_2 = "/grade/list2";
+
 	public static final String URL_COURSE_LIST = "/course/list";
 	public static final String URL_COURSE_LIST_2 = "/course/list2";
 	public static final String URL_COURSE_FIND = "/course/find";
@@ -166,6 +184,7 @@ public class Const {
 	public static final String URL_COURSE_EXPORT = "/course/export";
 	public static final String URL_COURSE_APPLY_DELETE = "/course/apply/delete";
 	public static final String URL_COURSE_MY = "/course/my";
+
 	public static final String URL_WEB_CONFIG_LIST = "/web/config/list";
 	public static final String URL_WEB_CONFIG_FIND = "/web/config/find";
 	public static final String URL_WEB_CONFIG_SAVE = "/web/config/save";
